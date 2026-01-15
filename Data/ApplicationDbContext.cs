bbba6a20d4c1169bbb7bea2e975019659abeb376
JP-Guillen <juan_guillen@ucne.edu.do>
2026-01-14 23:13:41 -0400

    feat ✨:Agrega Contexto

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
+using GestionEntradaInventario.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace GestionEntradaInventario.Data
+namespace GestionEntradaInventario.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
+
+    public DbSet<Productos> Productos { get; set; }
+    public DbSet<Entradas> Entradas { get; set; }
+    public DbSet<EntradasDetalle> EntradasDetalle { get; set; }
 }
